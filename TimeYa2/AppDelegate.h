035db954d716cf482354c796cd1d1bb9cf642306
PartyMan <partyman21@partymanmac.home>
2013-11-27 21:32:29 -0500

    Initial Commit

---
+++ TimeYa2/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TimeYa2
+//
+//  Created by PartyMan on 11/27/13.
+//  Copyright (c) 2013 PartyMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
